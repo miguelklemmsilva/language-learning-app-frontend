ee9127a77271eb69e810af7a0b35d041fbd41fa7
miguel4521 <109853127+miguel4521@users.noreply.github.com>
2023-08-17 10:04:14 +0100

    fixed cleanString removing all japanese characters

--- src/components/Practice/Question/Question.js
+++ src/components/Practice/Question/Question.js
@@ -21,7 +21,7 @@ const Question = ({sentence, setNextQuestion, sentenceNumber, updateSentence, se
 
     function cleanString(str) {
         return str
-            .replace(/[^a-zA-Z\u00C0-\u017F\s]/g, '')
+            .replace(/[^a-zA-Z\u00C0-\u017F\u3040-\u309F\u30A0-\u30FF\u4E00-\u9FAF\uFF00-\uFFEF\s]/g, '')
             .replace(/\s+/g, ' ')
             .toLowerCase()
             .trim();
