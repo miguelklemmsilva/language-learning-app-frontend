f34fd18daf0d143584608f6e59d497f211d88a05
miguel4521 <109853127+miguel4521@users.noreply.github.com>
2023-08-17 10:04:27 +0100

    made auth0 useable in both prod and dev

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ root.render(
         authorizationParams={{
             audience: "https://dev-k3jnyjgiqp886i26.uk.auth0.com/api/v2/",
             scope: "read:current_user update:current_user_metadata",
-            redirect_uri: "http://localhost:3000"
+            redirect_uri: process.env.REACT_APP_AUTH0_REDIRECT_URI
         }}
     >
         <App/>
